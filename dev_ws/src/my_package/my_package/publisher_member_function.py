1a0d23331d1daf8d865f244141b09d49b67ed512
DESKTOP-4885BJC\User <hiverlab@gmail.com>
2021-05-02 11:25:25 +0800

    updates

--- dev_ws/src/my_package/my_package/publisher_member_function.py
+++ dev_ws/src/my_package/my_package/publisher_member_function.py
@@ -1,16 +1,49 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from __future__ import print_function
+import argparse
+import math
+import time
+import sys
+import random
+import pandas as pd
+from signal import signal, SIGINT
+from triad_openvr import triad_openvr
+from pythonosc import osc_message_builder
+from pythonosc import osc_bundle_builder
+from pythonosc import udp_client
+from reprint import output
+from colorama import Fore, Back, Style
+
+# Initialize Tria's OpenVR wrapper and print discovered objects
+v = triad_openvr.triad_openvr()
+v.print_discovered_objects()
+
+# Sort through all discovered devices and keep track by type
+deviceCount = 0
+devices = {
+    'tracker': [],
+    'hmd': [],
+    'controller': [],
+    'tracking reference': []
+}
+
+for deviceName, device in v.devices.items():
+    device._id = deviceName.split("_").pop()
+    devices[device.device_class.lower()].append(device)
+    deviceCount += 1
+
+# pose tracking interval
+interval = 1/250
+
+ip = "127.0.0.1"
+port = 7000
+track = ["hmd", "tracker", "controller"]
+freq = 250
+mode = ['euler', 'quaternion']
+
+# initialize OSC client
+client = udp_client.SimpleUDPClient(ip, port)
+
+
 
 import rclpy
 from rclpy.node import Node
@@ -24,8 +57,8 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(Hmd, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        # timer_period = 0.5  # seconds
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
